ca2720a69865d9e2462d270ebc57e70c5e4dde0a
isabelamenezes <isabellamenezes@outlook.com>
2025-09-29 22:51:46 -0300

    Adicionando nivelmestre.c

--- OlaMundo/n1_nc4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-struct CartaSuperTrunfo {
-   char estado;
-   char codigo[4]; // Supondo um comprimento máximo de 3 caracteres + ''
-   char cidade[50];
-   int populacao;
-};
